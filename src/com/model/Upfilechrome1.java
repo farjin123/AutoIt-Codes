6638e0edec5d4fe7e81d4950d6dff3cc6a22c234
SHAZEEN <farjin94@gmail.com>
2019-09-16 15:55:15 +0530

    First commit

---
+++ src/com/model/Upfilechrome1.java
@@ -0,0 +1,23 @@
+package com.model;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Upfilechrome1 {
+
+	public static void main(String[] args) throws IOException {
+		
+		System.setProperty("webdriver.chrome.driver", "G:\\Mams lecture practicals\\selenium exes file and jar files\\chromedriver_win32 (1)\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("file:///G:/AutoIT/AutoData/upload1.html");
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("//input[@type='file']")).click();
+		Runtime.getRuntime().exec("G:\\AutoIT\\AutoData\\fileup1.exe");
+		
+
+	}
+
+}

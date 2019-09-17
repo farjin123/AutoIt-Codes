4d44deb28824a43715852566cf12b3bd0df7c271
farjin123 <55377196+farjin123@users.noreply.github.com>
2019-09-17 20:29:00 +0530

    changes in uploading the file
    making changes

--- src/com/model/Upfilechrome1.java
+++ src/com/model/Upfilechrome1.java
@@ -10,6 +10,7 @@ public class Upfilechrome1 {
 
 	public static void main(String[] args) throws IOException {
 		
+		System.out.println("transfering the code from github to eclipse");
 		System.out.println("auto it git hub");
 		System.setProperty("webdriver.chrome.driver", "G:\\Mams lecture practicals\\selenium exes file and jar files\\chromedriver_win32 (1)\\chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
